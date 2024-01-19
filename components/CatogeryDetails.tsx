960578c54c5add3aad8864389b8a39f725d844b0
Ria <riaratnasari@outlook.com>
2024-01-19 22:53:09 +0100

    add category details content

--- components/CatogeryDetails.tsx
+++ components/CatogeryDetails.tsx
@@ -1,12 +1,67 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import {Pressable, StyleSheet, Text, TextInput, View} from 'react-native';
 
-const CatogeryDetails = () => {
+interface CategoryDetailsProps {
+  category: string;
+  onPress: () => void;
+}
+const CatogeryDetails = (props: CategoryDetailsProps) => {
   return (
     <View>
-      <Text>Category Details</Text>
+      <Text>{props.category}</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Budget"
+        keyboardType="numeric"
+      />
+
+      <Pressable style={styles.button} onPress={props.onPress}>
+        <Text style={styles.text}>{'Save'}</Text>
+      </Pressable>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    justifyContent: 'center',
+    textAlign: 'center',
+    color: '#656565',
+    paddingBottom: 40,
+  },
+
+  input: {
+    height: 40,
+    margin: 12,
+    borderRadius: 16,
+    padding: 12,
+    width: 250,
+    letterSpacing: 0.25,
+    backgroundColor: '#f4f5fb',
+  },
+
+  button: {
+    height: 40,
+    margin: 12,
+    borderRadius: 16,
+    paddingHorizontal: 12,
+    width: 250,
+    lineHeight: 21,
+    letterSpacing: 0.25,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#F58B32',
+  },
+
+  text: {
+    fontSize: 16,
+    lineHeight: 21,
+    letterSpacing: 0.25,
+    color: 'white',
+    fontWeight: 'bold',
+  },
+});
 
 export default CatogeryDetails;
