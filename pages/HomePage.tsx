b9d20966893568a5e0751f3ebf28a105058967ee
Ria <riaratnasari@outlook.com>
2024-01-19 23:39:43 +0100

    add bottom tabs

--- pages/HomePage.tsx
+++ pages/HomePage.tsx
@@ -4,20 +4,36 @@ import CatogeryDetails from '../components/CatogeryDetails';
 import BudgetPlanning from './BudgetPlanning';
 import InputExpensePage from './InputExpensePage';
 import SummaryPage from './SummaryPage';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
-const HomePage = () => {
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+
+const Tab = createBottomTabNavigator();
+
+function HomePage() {
   return (
-    <View>
-      <Text> Homepage</Text>
-      {/* <Text>Expense Manager</Text> */}
-      {/* <LoginPage /> */}
-      {/* <InputExpensePage /> */}
-      {/* <BudgetPlanning /> */}
-      {/* <HomePage /> */}
-      {/* <CatogeryDetails /> */}
-      {/* <SummaryPage /> */}
-    </View>
+    <Tab.Navigator
+      screenOptions={({route}) => ({
+        headerShown: false,
+        tabBarIcon: ({focused, color, size}) => {
+          let iconName;
+          if (route.name === 'InputExpense') {
+            iconName = focused ? 'ios-home' : 'ios-home-outline';
+          } else if (route.name === 'SummaryPage') {
+            iconName = focused ? 'ios-home' : 'ios-home-outline';
+          } else if (route.name === 'BudgetPlanning') {
+            iconName = focused ? 'ios-home' : 'ios-home-outline';
+          }
+          return <Ionicons name={iconName} size={size} color={color} />;
+        },
+        tabBarActiveTintColor: '#F58B32',
+        tabBarInactiveTintColor: 'gray',
+      })}>
+      <Tab.Screen name="InputExpense" component={InputExpensePage} />
+      <Tab.Screen name="SummaryPage" component={SummaryPage} />
+      <Tab.Screen name="BudgetPlanning" component={BudgetPlanning} />
+    </Tab.Navigator>
   );
-};
+}
 
 export default HomePage;
