9dd3c8777bf84e41baa8273591a6182525a4b4d6
Ria <riaratnasari@outlook.com>
2024-01-21 23:30:30 +0100

    add expense details styling

--- pages/ExpenseDetails.tsx
+++ pages/ExpenseDetails.tsx
@@ -1,12 +1,31 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import {Text, View, StyleSheet} from 'react-native';
 
 const ExpenseDetails = () => {
   return (
-    <View>
-      <Text>Expense Details</Text>
+    <View style={styles.pageContainer}>
+      <Text style={styles.title}>Expense Details</Text>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  pageContainer: {
+    marginLeft: 16,
+    marginRight: 16,
+    marginTop: 24,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    justifyContent: 'center',
+    textAlign: 'center',
+    color: '#656565',
+    paddingBottom: 40,
+  },
+});
 
 export default ExpenseDetails;
